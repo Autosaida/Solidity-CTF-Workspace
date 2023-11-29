3bf41ab2d1e91ff94bfb9aa3ccb45e6a4be46ed1
Autosaida <873666875@qq.com>
2023-11-29 15:31:34 +0800

    change to foundry

--- contracts/MetaTrustCTF_2023/EthStaking/interface/IInsurance.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-interface IInsurance {
-    function registerContract() external;
-    function unregisterContract() external;
-    function requestCompensation(uint256 shortfall) external;
-    function withdraw() external;
-}
--- contracts/SEETF_2023/PigeonVault/interfaces/IOwnershipFacet.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
-
-interface IOwnershipFacet {
-    function owner() external view returns (address);
-    function transferOwnership(address _newOwner) external;
-}
